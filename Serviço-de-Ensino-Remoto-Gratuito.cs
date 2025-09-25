b55e7ebfbb5b9e18b00b7e4143ba2c34080698c2
fdechico <fdechico7@gmail.com>
2025-09-25 10:42:03 -0300

    Add files via upload

---
+++ Serviço-de-Ensino-Remoto-Gratuito.cs
@@ -0,0 +1,33 @@
+# Nome do Projeto: Serviço de Ensino Remoto Gratuito
+
+## Integrantes da Equipe
+- Fhelipe Vinicios Dechico
+
+## ODS Selecionado ou Instituição Atendida
+- ODS 1: Educação de Qualidade
+- ODS 2: Promoção da Igualdade de Oportunidades
+- Instituição: UTFPR - Universidade Tecnologia Federal do Paraná
+
+## Descrição do Projeto
+Desenvolver uma aplicação web que funcione como uma plataforma de ensino remoto gratuito, oferecendo cursos e materiais educativos acessíveis a todos. O projeto busca atender a um Objetivo de Desenvolvimento Sustentável (ODS) relacionado à educação de qualidade e à promoção da igualdade de oportunidades.
+
+## Estrutura do Projeto
+
+Serviço-de-Ensino-Remoto-Gratuito-/
+├── index.html 
+# - Cabeçalho e Rodapé: Incluindo o nome da plataforma e links para as principais seções.
+# - Menu de Navegação: Links para as páginas de cursos, cadastro e contato.
+├── css/
+│ └── styles.css 
+# - Ícones: Dois ou mais ícones representando áreas de conhecimento (como ciência, arte, tecnologia).
+# - Caixas de Texto: Informações sobre a missão da plataforma e depoimentos de usuários.
+# - Links: Três ou mais links para acessar diretamente os cursos ou materiais de estudo.
+# - Cores e Fontes: Uso de pelo menos três cores e duas fontes diferentes para manter a estética
+e a legibilidade.
+├── js/
+│ └── script.js 
+# - HTML e CSS: Para construção das páginas, assegurando a semântica e validação dos códigos.
+# - Validação: O código-fonte será validado conforme os padrões do W3C.
+└── assets/
+└── images/ 
+# - Imagens: Três ou mais imagens ilustrativas dos cursos oferecidos.
